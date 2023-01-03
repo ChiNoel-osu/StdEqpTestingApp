39ef31abfda0a5f9f2f759d3cd9348c53d3b3ac6
Victor <1606427136@qq.com>
2023-01-03 21:00:05 +0800

    plot test

---
+++ Model/CustomThemeColors.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Media;
+
+namespace StdEqpTesting.Model
+{
+	public static class CustomThemeColors
+	{
+		public static class DarkTheme
+		{
+			public static readonly SolidColorBrush BackgroundBrush = new SolidColorBrush(Color.FromRgb(30,30,30));
+			public static readonly SolidColorBrush ForegroundBrush = new SolidColorBrush(Colors.White);
+			public static readonly SolidColorBrush CaretBrush = new SolidColorBrush(Colors.WhiteSmoke);
+		}
+		public static class LightTheme
+		{
+			public static readonly SolidColorBrush BackgroundBrush = new SolidColorBrush(Colors.White);
+			public static readonly SolidColorBrush ForegroundBrush = new SolidColorBrush(Colors.Black);
+			public static readonly SolidColorBrush CaretBrush = new SolidColorBrush(Colors.Black);
+		}
+	}
+}
--- ViewModel/MainWindowVM.cs
+++ ViewModel/MainWindowVM.cs
@@ -1,5 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using OxyPlot;
+using OxyPlot.Annotations;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using StdEqpTesting.Model;
 using StdEqpTesting.View;
+using System;
 using System.Windows;
 
 namespace StdEqpTesting.ViewModel
@@ -12,6 +18,33 @@ namespace StdEqpTesting.ViewModel
 			LoginWindow loginWindow = new LoginWindow();
 			parent.Close();
 			loginWindow.Show();
+		}
+
+		public PlotModel Plot1 { get; set; } = new PlotModel()
+		{
+			Title = "DAmn",
+			Subtitle = "piss",
+			SubtitleFont = "Comic Sans MS",
+			SubtitleFontSize = 20,
+			PlotType = PlotType.Cartesian,
+			TitleColor = OxyColors.White,
+			SubtitleColor = OxyColors.Lime,
+			PlotAreaBorderColor = OxyColors.White
+		};
+
+		public MainWindowVM()
+		{
+			Plot1.Annotations.Add(new LineAnnotation
+			{
+				StrokeThickness = 2,
+				Color = OxyColors.Gray,
+				Type = LineAnnotationType.Horizontal,
+				LineStyle = LineStyle.LongDashDot,
+				Y = 0
+			});
+			Plot1.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, TicklineColor = OxyColors.White, TextColor = OxyColors.White });
+			Plot1.Axes.Add(new LinearAxis { Position = AxisPosition.Left, TicklineColor = OxyColors.White, TextColor = OxyColors.White });
+			Plot1.Series.Add(new FunctionSeries(Math.Cos, -10, 10, 0.01, title: "BIG"));
 		}
 	}
 }
